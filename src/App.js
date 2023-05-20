a20261152bc521c846997c197370a0924817b6f1
Gevorg <petrossian.gev@gmail.com>
2023-05-20 12:09:07 +0400

    redux toolkit done

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 import { useEffect } from "react";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals,getCartItems } from "./features/cart/cartSlice";
 import Modal from "./components/Modal";
 
 
 function App() {
-   const dispatch = useDispatch();
-   const { cartItems } = useSelector((store) => store.cart);
-   const { isOpen } = useSelector(store => store.modal);
-   useEffect(() => {
-			dispatch(calculateTotals());
-   },[cartItems]);
-   
-      useEffect(() => {}, [isOpen]);
+	const dispatch = useDispatch();
+	const { cartItems, isLoading } = useSelector((store) => store.cart);
+	const { isOpen } = useSelector(store => store.modal);
+
+	useEffect(() => {
+		dispatch(calculateTotals());
+	}, [cartItems]);
+
+	useEffect(()=>{
+		dispatch(getCartItems("test"));
+	},[])
+
+	if (isLoading) {
+		return (
+		  <div className='loading'>
+			<h1>Loading...</h1>
+		  </div>
+		);
+	  }
+
+
 	return (
 		<main>
 			{isOpen && <Modal />}
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,28 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { openModal } from "../modal/modalSlice";
 
+
+const url = 'https://course-api.com/react-useReducer-cart-project1';
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', async (name, thunkAPI) => {
+    try {
+        // console.log("name.",name,thunkAPI,thunkAPI.getState());
+        thunkAPI.dispatch(openModal())
+        const response = await axios(url);
+        return response.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message || "Something went wrong")
+    }
+})
 const initialState = {
-	cartItems,
-	amount: 0,
-	total: 0,
-	isLoading: true,
+    cartItems: [],
+    amount: 0,
+    total: 0,
+    isLoading: true,
 };
 
 const cartSlice = createSlice({
-	name: "cart",
-	initialState,
-	reducers: {
-		clearCart: (state) => {
-			state.cartItems = [];
-		},
-		removeItem: (state, action) => {
-			state.cartItems = state.cartItems.filter((item) => item.id !== action.payload);
+    name: "cart",
+    initialState,
+    reducers: {
+        clearCart: (state) => {
+            state.cartItems = [];
         },
-        increaseItem:(state,{payload}) => {
+        removeItem: (state, action) => {
+            state.cartItems = state.cartItems.filter((item) => item.id !== action.payload);
+        },
+        increaseItem: (state, { payload }) => {
             let item = state.cartItems.find(item => item.id === payload);
             ++item.amount;
         },
-        decreaseItem: ({ cartItems },{ payload }) => {
+        decreaseItem: ({ cartItems }, { payload }) => {
             let item = cartItems.find(item => item.id === payload.id);
             --item.amount;
         },
@@ -30,15 +44,26 @@ const cartSlice = createSlice({
             let amount = 0;
             let total = 0;
             state.cartItems.forEach((item) => {
-                console.log()
                 amount += item.amount;
                 total += item.price * amount;
             });
             state.amount = amount;
             state.total = total;
-
         }
-	},
+    },
+    extraReducers: {
+        [getCartItems.pending]: state => {
+            state.isLoading = true;
+        },
+        [getCartItems.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.cartItems = action.payload;
+        },
+        [getCartItems.rejected]:(state, action) => {
+            // console.log("action",action.payload)
+            state.isLoading = false;
+        }
+    }
 });
 
 export const { clearCart, removeItem, increaseItem, decreaseItem, calculateTotals } = cartSlice.actions;
